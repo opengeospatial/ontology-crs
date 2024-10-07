abf680615d41e1fe63764035c956d37378e9afdd
Timo <situx@users.noreply.github.com>
2024-10-07 20:57:33 +0200

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -3,6 +3,12 @@ from rdflib.namespace import RDF, RDFS, OWL, SKOS, VANN, XSD
 import csv
 import os
 
+extontmapping={
+"srs.csv":"core.ttl",
+"cs.csv":"cs.ttl",
+"projections.csv":"projections.ttl"
+}
+
 g = Graph()
 g.bind("geosrs", "http://www.opengis.net/ont/srs/") 
 g.bind("skos","http://www.w3.org/2004/02/skos/core#")
@@ -16,6 +22,8 @@ geocrsNS="http://www.opengis.net/ont/srs/"
 
 dirname = os.path.dirname(__file__)
 abspath = os.path.join(dirname, '../csv/class/')
+
+
 
 directory = os.fsencode(abspath)
     
@@ -64,5 +72,30 @@ for file in os.listdir(directory):
                         g.add((URIRef(geocrsNS+row["Concept"]),RDFS.domain,URIRef(row["Domain"].replace("geosrs:",geocrsNS))))
     else:
         continue
+
 print(len(g))
-g.serialize(destination="geosrs.ttl")
+g.serialize(destination="index.ttl")
+       
+g=Graph() 
+g.bind("ign","http://data.ign.fr/def/ignf#")      
+g.bind("iso19112","http://def.isotc211.org/iso19112/2019/SpatialReferencingByGeographicIdentifier#")   
+g.bind("geosrs", "http://www.opengis.net/ont/srs/")  
+dirname = os.path.dirname(__file__)
+abspath = os.path.join(dirname, '../csv/alignment/')
+directory = os.fsencode(abspath)
+print(abspath)    
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".csv"): 
+        with open(abspath+filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            objprop=False
+            if "obj" in filename:
+                objprop=True
+            for row in reader:
+                if "Concept source" in row and row["Concept source"]!="" and "Concept target" in row and row["Concept target"]!="" and "Property" in row and row["Property"]!="":
+                    g.add((URIRef(row["Concept source"].replace("geosrs:",geocrsNS).replace("ign:","http://data.ign.fr/def/ignf#").replace("iso19112:","http://def.isotc211.org/iso19112/2019/SpatialReferencingByGeographicIdentifier#")),URIRef(row["Property"].replace("owl:","http://www.w3.org/2002/07/owl#").replace("rdfs:","http://www.w3.org/2000/01/rdf-schema#")),URIRef(geocrsNS+row["Concept target"].replace("geosrs:",geocrsNS).replace("ign:","http://data.ign.fr/def/ignf#").replace("iso19112:","http://def.isotc211.org/iso19112/2019/SpatialReferencingByGeographicIdentifier#")))
+    else:
+        continue
+
+g.serialize(destination="alignments.ttl")

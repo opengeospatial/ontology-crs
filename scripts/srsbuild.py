35f4a3831c94d22600a7cbda9b6a97e2651be76f
Timo <situx@users.noreply.github.com>
2024-10-25 19:33:47 +0200

    support for extensions

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -3,20 +3,18 @@ from rdflib.namespace import RDF, RDFS, OWL, SKOS, VANN, XSD
 import csv
 import os
 
-extontmapping={
-"srs.csv":"core.ttl",
-"cs.csv":"cs.ttl",
-"projections.csv":"projections.ttl"
-}
+exont={}
 
-g = Graph()
-g.bind("geosrs", "http://www.opengis.net/ont/srs/") 
-g.bind("skos","http://www.w3.org/2004/02/skos/core#")
+gcore = Graph()
+gcore.bind("geosrs", "http://www.opengis.net/ont/srs/") 
+gcore.bind("skos","http://www.w3.org/2004/02/skos/core#")
 
-g.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),RDF.type,OWL.Ontology))
-g.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),RDFS.label,Literal("SRS Ontology",lang="en")))
-g.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),VANN.preferredNamespacePrefix,Literal("geosrs",datatype=XSD.string)))
-g.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),VANN.preferredNamespaceUri,Literal("http://www.opengis.net/ont/srs/",datatype=XSD.anyURI)))
+gcore.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),RDF.type,OWL.Ontology))
+gcore.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),RDFS.label,Literal("SRS Ontology",lang="en")))
+gcore.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),VANN.preferredNamespacePrefix,Literal("geosrs",datatype=XSD.string)))
+gcore.add((URIRef("http://www.opengis.net/ont/srs/geosrs"),VANN.preferredNamespaceUri,Literal("http://www.opengis.net/ont/srs/",datatype=XSD.anyURI)))
+
+
 
 geocrsNS="http://www.opengis.net/ont/srs/"
 
@@ -29,20 +27,43 @@ directory = os.fsencode(abspath)
     
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
+    g = Graph()
+    exont[filename.replace(".csv","")]=g
+    curprefix="geo"+filename.replace(".csv","")
+    curns="http://www.opengis.net/ont/srs/"+filename.replace(".csv","")+"/"
+    g.bind(curprefix,curns) 
+    g.bind("skos","http://www.w3.org/2004/02/skos/core#")
+
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),RDF.type,OWL.Ontology))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),RDFS.label,Literal("SRS Ontology",lang="en")))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),VANN.preferredNamespacePrefix,Literal(curprefix,datatype=XSD.string)))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),VANN.preferredNamespaceUri,Literal("http://www.opengis.net/ont/srs/"+filename.replace(".csv","")+"/",datatype=XSD.anyURI)))
     if filename.endswith(".csv"): 
         with open(abspath+filename, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 if "Concept" in row and row["Concept"]!="":
-                    g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDF.type,OWL.Class))
-                    if "Label" in row and row["Label"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDFS.label,Literal(row["Label"],lang="en")))
-                    if "Definition" in row and row["Definition"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),SKOS.definition,Literal(row["Definition"],lang="en")))
-                    if "SubClass" in row and row["SubClass"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDFS.subClassOf,URIRef(row["SubClass"].replace("geosrs:",geocrsNS))))
+                    core=False
+                    if "Core Class?" in row and row["Core Class?"]=="Core Ontology":
+                        core=True
+                        gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.Class))
+                        if "Label" in row and row["Label"]!="":
+                            gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.label,Literal(row["Label"],lang="en")))
+                        if "Definition" in row and row["Definition"]!="":
+                            gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),SKOS.definition,Literal(row["Definition"],lang="en")))
+                        if "SubClass" in row and row["SubClass"]!="":
+                            gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.subClassOf,URIRef(row["SubClass"].replace("geosrs:",geocrsNS))))
+                    else:
+                        g.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.Class))
+                        if "Label" in row and row["Label"]!="":
+                            g.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.label,Literal(row["Label"],lang="en")))
+                        if "Definition" in row and row["Definition"]!="":
+                            g.add((URIRef(row["Concept"].replace(curprefix+":",curns)),SKOS.definition,Literal(row["Definition"],lang="en")))
+                        if "SubClass" in row and row["SubClass"]!="":
+                            g.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.subClassOf,URIRef(row["SubClass"].replace("geosrs:",geocrsNS))))                     
     else:
         continue
+ 
 
 dirname = os.path.dirname(__file__)
 abspath = os.path.join(dirname, '../csv/prop/')
@@ -50,6 +71,17 @@ directory = os.fsencode(abspath)
 print(abspath)    
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
+    g = Graph()
+    curprefix="geo"+filename.replace(".csv","")
+    curns="http://www.opengis.net/ont/srs/"+filename.replace(".csv","")+"/"
+    g.bind(curprefix, curns) 
+    g.bind("skos","http://www.w3.org/2004/02/skos/core#")
+
+
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),RDF.type,OWL.Ontology))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),RDFS.label,Literal("SRS Ontology",lang="en")))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),VANN.preferredNamespacePrefix,Literal(curprefix,datatype=XSD.string)))
+    g.add((URIRef("http://www.opengis.net/ont/srs/geosrs/"+filename.replace(".csv","")),VANN.preferredNamespaceUri,Literal(curns,datatype=XSD.anyURI)))
     if filename.endswith(".csv"): 
         with open(abspath+filename, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -57,24 +89,45 @@ for file in os.listdir(directory):
             if "obj" in filename:
                 objprop=True
             for row in reader:
+                print(row)
                 if "Concept" in row and row["Concept"]!="":
-                    if objprop:
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDF.type,OWL.ObjectProperty))
-                    else:
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDF.type,OWL.DatatypeProperty))
-                    if "Label" in row and row["Label"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDFS.label,Literal(row["Label"],lang="en")))
-                    if "Definition" in row and row["Definition"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),SKOS.definition,Literal(row["Definition"],lang="en")))
-                    if "Range" in row and row["Range"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDFS.range,URIRef(row["Range"].replace("geosrs:",geocrsNS))))
-                    if "Domain" in row and row["Domain"]!="":
-                        g.add((URIRef(row["Concept"].replace("geosrs:",geocrsNS)),RDFS.domain,URIRef(row["Domain"].replace("geosrs:",geocrsNS))))
+                    if "Core Property?" in row:
+                        if row["Core Property?"]=="Core Ontology":
+                            core=True
+                            if objprop:
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.ObjectProperty))
+                            else:
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.DatatypeProperty))
+                            if "Label" in row and row["Label"]!="":
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.label,Literal(row["Label"],lang="en")))
+                            if "Definition" in row and row["Definition"]!="":
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),SKOS.definition,Literal(row["Definition"],lang="en")))
+                            if "Range" in row and row["Range"]!="":
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.range,URIRef(row["Range"].replace("geosrs:",geocrsNS))))
+                            if "Domain" in row and row["Domain"]!="":
+                                gcore.add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.domain,URIRef(row["Domain"].replace("geosrs:",geocrsNS))))
+                        else:
+                            if row["Core Property?"].lower() in exont:
+                                if objprop:
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.ObjectProperty))
+                                else:
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDF.type,OWL.DatatypeProperty))
+                                if "Label" in row and row["Label"]!="":
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.label,Literal(row["Label"],lang="en")))
+                                if "Definition" in row and row["Definition"]!="":
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),SKOS.definition,Literal(row["Definition"],lang="en")))
+                                if "Range" in row and row["Range"]!="":
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.range,URIRef(row["Range"].replace("geosrs:",geocrsNS))))
+                                if "Domain" in row and row["Domain"]!="":
+                                    exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(curprefix+":",curns)),RDFS.domain,URIRef(row["Domain"].replace("geosrs:",geocrsNS))))
+            g.serialize(destination=filename.replace(".csv","")+".ttl") 
     else:
         continue
 
 print(len(g))
-g.serialize(destination="index.ttl")
+for item in exont:
+    exont[item].serialize(destination=item+".ttl") 
+gcore.serialize(destination="index.ttl")
        
 g=Graph() 
 g.bind("ign","http://data.ign.fr/def/ignf#")      

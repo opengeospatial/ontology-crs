8fe88a4a9dc9ff95d0ee8fbef2327ee99529cb0b
Timo Homburg <situx@users.noreply.github.com>
2025-07-12 22:20:01 +0200

    Update srsbuild.py

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -40,7 +40,7 @@ moduleToRequirements={"06-core.adoc":{},"07-co_extension.adoc":{},"08-cs_extensi
 
 prefixtoclasses={"geosrs":[]}
 requirementsToClasses={}
-prefixtoproperties={"geosrs":[],"CS":[],"CO":[],"DATUM":[],"projection":[]}
+prefixtoproperties={"geosrs":[],"SRS":[],"CS":[],"CO":[],"DATUM":[],"projection":[]}
 classToPrefix={}
 prefixToModule={"srs":"06-core.adoc","core":"06-core.adoc","co":"07-co_extension.adoc","cs":"08-cs_extension.adoc","datum":"09-datum_extension.adoc","srsapplication":"10-srsapplication_extension.adoc","projection":"11-projections_extension.adoc","planet":"12-planet_extension.adoc"}
 moduleToAdoc={"06-core.adoc":{},"07-co_extension.adoc":{},"08-cs_extension.adoc":{},"09-datum_extension.adoc":{},"10-srsapplication_extension.adoc":{},"11-projections_extension.adoc":{},"12-planet_extension.adoc":{}}

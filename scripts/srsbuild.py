215a29d5d82526536b82f2d7981bf005a31200c6
Timo Homburg <situx@users.noreply.github.com>
2025-08-09 00:44:10 +0200

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -565,7 +565,7 @@ for ad in moduleToAdoc:
 	with open("spec/sections/"+ad,"w") as file:
 		file.write(content)             
 	with open("spec/sections/"+ad.replace(".adoc","_classes.adoc"), 'w',encoding="utf-8") as f:
-        print(moduleToRequirements[ad])
+		print(moduleToRequirements[ad])
 		reqs=moduleToRequirements[ad]
 		#print(reqs)
 		if len(reqs)>0:

aae5ed1a8c8b2317f386323116390bfd7acfbabe
Timo Homburg <situx@users.noreply.github.com>
2025-08-09 02:09:39 +0200

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -362,8 +362,8 @@ for file in os.listdir(directory):
                                     exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(coreprefix+":",curns+str(row["Core Property?"]).lower()+"/")),RDFS.label,Literal(row["Label"],lang="en")))
                                 if "Requirement" in row and row["Requirement"]!="" and str(row["Core Property?"]).lower() in prefixToModule:
                                     if row["Requirement"] not in moduleToRequirements[prefixToModule[str(row["Core Property?"]).lower()]]:
-                                        moduleToRequirements[prefixToModule[str(row["Core Property?"]).lower(]][row["Requirement"]]=[]
-                                    moduleToRequirements[prefixToModule[str(row["Core Property?"]).lower(]][row["Requirement"]].append(row["Concept"])
+                                        moduleToRequirements[prefixToModule[str(row["Core Property?"]).lower()]][row["Requirement"]]=[]
+                                    moduleToRequirements[prefixToModule[str(row["Core Property?"]).lower()]][row["Requirement"]].append(row["Concept"])
                                 if "Definition" in row and row["Definition"]!="":
                                     exont[row["Core Property?"].lower()].add((URIRef(row["Concept"].replace(coreprefix+":",curns+str(row["Core Property?"]).lower()+"/")),SKOS.definition,Literal(row["Definition"],lang="en")))
                                     adocdef+="|Definition\n|"+str(row["Definition"])+"\n\n"

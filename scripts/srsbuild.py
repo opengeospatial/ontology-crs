29c7853b9c1006bb211933ee3da6dfc07f15c726
Timo Homburg <situx@users.noreply.github.com>
2025-08-10 13:34:56 +0200

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -45,7 +45,7 @@ prefixtoclasses={"geosrs":[]}
 requirementsToClasses={}
 prefixtoproperties={"geosrs":[],"SRS":[],"CS":[],"CO":[],"DATUM":[],"projection":[]}
 classToPrefix={}
-prefixToModule={"srs":"06-core.adoc","core":"06-core.adoc","co":"07-co_extension.adoc","cs":"08-cs_extension.adoc","datum":"09-datum_extension.adoc","srsapplication":"10-srsapplication_extension.adoc","projection":"11-projections_extension.adoc","planet":"12-planet_extension.adoc"}
+prefixToModule={"srs":"06-core.adoc","core":"06-core.adoc","co":"07-co_extension.adoc","cs":"08-cs_extension.adoc","datum":"09-datum_extension.adoc","application":"10-srsapplication_extension.adoc","srsapplication":"10-srsapplication_extension.adoc","projection":"11-projections_extension.adoc","planet":"12-planet_extension.adoc"}
 moduleToAdoc={"06-core.adoc":{},"07-co_extension.adoc":{},"08-cs_extension.adoc":{},"09-datum_extension.adoc":{},"10-srsapplication_extension.adoc":{},"11-projections_extension.adoc":{},"12-planet_extension.adoc":{}}
 
 galigns=Graph() 

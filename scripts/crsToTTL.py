bc324bb27b071f1c8c44afe40f3aaafd5b62f3eb
Timo Homburg <situx@users.noreply.github.com>
2025-08-13 00:55:05 +0200

    fixes

--- scripts/crsToTTL.py
+++ scripts/crsToTTL.py
@@ -248,7 +248,7 @@ def crsToTTL(ttl,curcrs,x,geodcounter,crsclass):
 					if prj in proj4string:
 						ttl.add("geosrsoperation:"+str(coordoperationid)+" rdf:type "+projections[prj]+" . \n")
 						ttl.add(str(projections[prj])+" <http://xmlns.com/foaf/0.1/image> \"https://situx.github.io/proj4rdf/projections/"+str(projections[prj]).replace("geosrs:","")+".svg\"^^xsd:anyURI .")
-						examples[projections[prj]]=websitensshort+"/crs/operation/"+str(coordoperationid)
+						examples[projections[prj]]=websitensshort+"/operation/"+str(coordoperationid)
 						found=True
 						break
 			if not found:

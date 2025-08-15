25ec0d31d009bc8880dd3516550c13ea8d72bcc8
Timo Homburg <situx@users.noreply.github.com>
2025-08-15 15:34:52 +0200

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -663,7 +663,7 @@ for ad in moduleToAdoc:
 				print(str(ad)+" - "+str(req))
 				print(moduleToRequirements[ad][req])
 				if req in reqToDesc:
-					f.write(reqToDesc[req+"\n\n"])
+					f.write(reqToDesc[req]+"\n\n")
 				for cls in moduleToRequirements[ad][req]:
 					print(str(req)+" - "+str(cls)+" "+cls.replace("geosrs:","")+" "+str(cls.replace("geosrs:","") in moduleToAdoc[ad]))
 					if cls.replace("geosrs:","") in moduleToAdoc[ad]:

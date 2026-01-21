5c6e286f3f696c20b86241045a25908232801d2e
Timo Homburg <situx@users.noreply.github.com>
2026-01-21 12:14:51 +0100

    fixes

--- scripts/srsbuild.py
+++ scripts/srsbuild.py
@@ -115,6 +115,7 @@ def convertCSVToSHACLAndADoc():
             curshaclres.bind("sh","http://www.w3.org/ns/shacl#")
             curshaclres.bind("sf","http://www.opengis.net/ont/sf#")
             curshaclres.bind("rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+            alreadyprocessed=set()
             with open(abspath+filename, newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 adocdef+="===== SHACL Shapes: "+str(filename).replace(".csv","").capitalize()+"\n\n"
@@ -122,8 +123,8 @@ def convertCSVToSHACLAndADoc():
                 adocdef+="|Label|TargetNode|Property|Class|MinCount|MaxCount|Comment\n\n"
                 for row in reader:
                     if "Concept" in row and row["Concept"]!="":
-                        shapeuri=row["Concept"].replace("geosrs:","https://w3id.org/geosrs/")+"Shape"
-                        shapepropuri=row["Concept"].replace("geosrs:","https://w3id.org/geosrs/")+"Shape_Property"
+                        shapeuri=row["Concept"].replace("geosrs:","https://w3id.org/geosrs/")+"_"+str(row["Property"])+"_Shape"
+                        shapepropuri=row["Concept"].replace("geosrs:","https://w3id.org/geosrs/")+"_"+str(row["Property"])+"_Shape_Property"
                         shaclres.add((URIRef(shapeuri),URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),URIRef("http://www.w3.org/ns/shacl#NodeShape")))
                         shaclres.add((URIRef(shapeuri),URIRef("http://www.w3.org/2000/01/rdf-schema#label"),Literal("CRS Ontology Shape S"+str(shapecounter),lang="en")))
                         shaclres.add((URIRef(shapeuri),URIRef("http://www.w3.org/ns/shacl#targetNode"),URIRef(row["Concept"].replace("geosrs:","https://w3id.org/geosrs/"))))
